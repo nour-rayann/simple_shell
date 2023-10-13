2f4bb08407c611ec41fa6fa50a20e3987aa7f7ea
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-10-13 23:16:20 +0000

    header_update

--- main.h
+++ main.h
@@ -11,6 +11,8 @@
 #include <sys/stat.h>
 
 int main(int ac, char **argv);
+
+/* custom functions */
 char *_strcpy(char *line_ptr);
 char *_strcpy2(char *dest, char *src);
 int _strlen(char *str);
@@ -19,12 +21,16 @@ char *_strdup(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _strcmp(char *s1, char *s2);
 
-/** main function **/
+/* token handling */
 int num_tokens(char *line_ptr);
 char **create_tokens(char *lineptr);
+void free_tokens(char **tokens);
+
+/* command execution */
 void execute_command(char **argv);
 void execute(char *cmd, char **argv);
-void free_tokens(char **tokens);
+
+/* PATH handling */
 char *get_address(char *cmd_copy);
 char *check_dir(char *path_token, char *cmd_copy, int cmd_length);
 char *check_cmd(char *cmd_copy);
