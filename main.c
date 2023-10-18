46ff93bdf364e188c1b285fc4da76f30d9a81fbc
Nouran <no0ouranmohamed@gmail.com>
2023-10-18 09:51:49 +0300

    trying to fix compilation error

--- main.c
+++ main.c
@@ -9,12 +9,13 @@ int main(void)
 	size_t n;
 	char *line_ptr = NULL;
 	char **tokens;
-	int state;
+	int state, loop_count = 0;
 
 	while (1)
 	{
+		loop_count++;
 		if (isatty(STDIN_FILENO))
-			printf("simple_shell$ ");
+			printf("$ ");
 		num_char_read = getline(&line_ptr, &n, stdin);
 		if (num_char_read == -1)
 		{
@@ -23,16 +24,19 @@ int main(void)
 			exit(0);
 		}
 		tokens = create_tokens(line_ptr);
-		if (_strcmp(tokens[0], "exit") == 0)
+		if (tokens && tokens[0])
 		{
+			if (_strcmp(tokens[0], "exit") == 0)
+			{
+				free_tokens(tokens);
+				free(line_ptr);
+				line_ptr = NULL;
+				exit(0);
+			}
+			execute_command(tokens);
 			free_tokens(tokens);
-			free(line_ptr);
-			line_ptr = NULL;
-			exit(0);
+			wait(&state);
 		}
-		execute_command(tokens);
-		free_tokens(tokens);
-		wait(&state);
 	}
 	free(line_ptr);
 	return (state);
