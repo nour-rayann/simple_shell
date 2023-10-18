0f9c5b293eb079139fe62f583a4fbafaac520947
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-10-18 06:18:37 +0000

    seg fault

--- functions1.c
+++ functions1.c
@@ -109,6 +109,9 @@ char *_strcat(char *dest, char *src)
 
 int _strcmp(char *s1, char *s2)
 {
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+
 	/* Make sure not at end of string */
 	while ((*s1 != '\0') || (*s2 != '\0'))
 	{
